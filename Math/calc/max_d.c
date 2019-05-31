50f3852a75e484372135c272c493bd7b888c72ee
Euan Hollidge <ehollidg@f0r1s3.codam.nl>
2019-05-31 14:30:59 +0200

    Min and Max

--- Math/calc/max_d.c
+++ Math/calc/max_d.c
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   math.h                                             :+:    :+:            */
+/*   max_d.c                                            :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/05/31 13:42:30 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/05/31 14:30:22 by ehollidg      ########   odam.nl         */
+/*   Created: 2019/05/31 14:29:35 by ehollidg       #+#    #+#                */
+/*   Updated: 2019/05/31 14:30:09 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATH_H
-# define MATH_H
-
-//Calc
-unsigned long	powlo(unsigned long base, unsigned long expo);
-unsigned int	powint(unsigned int base, unsigned int expo);
-unsigned long	sqrtlo(unsigned long nb);
-unsigned int	sqrtlo(unsigned int nb);
-long			min(long a, long b);
-double			min_d(double a, double b);
-long			max(long a, long b);
-double			max_d(double a, double b);
-
-#endif
+double		max_d(double a, double b)
+{
+	return (a > b ? a : b);
+}
--- Math/calc/min_d.c
+++ Math/calc/min_d.c
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   math.h                                             :+:    :+:            */
+/*   min_d.c                                            :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/05/31 13:42:30 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/05/31 14:30:22 by ehollidg      ########   odam.nl         */
+/*   Created: 2019/05/31 14:27:31 by ehollidg       #+#    #+#                */
+/*   Updated: 2019/05/31 14:28:08 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATH_H
-# define MATH_H
-
-//Calc
-unsigned long	powlo(unsigned long base, unsigned long expo);
-unsigned int	powint(unsigned int base, unsigned int expo);
-unsigned long	sqrtlo(unsigned long nb);
-unsigned int	sqrtlo(unsigned int nb);
-long			min(long a, long b);
-double			min_d(double a, double b);
-long			max(long a, long b);
-double			max_d(double a, double b);
-
-#endif
+double	min_d(double a, double b)
+{
+	return (a < b ? a : b);
+}
