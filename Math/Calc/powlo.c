94334ef190bf08f7e47f5cc758cc5edbd21b8f3c
Euan Hollidge <ehollidg@f0r1s3.codam.nl>
2019-05-31 13:50:41 +0200

    powlo

--- Math/Calc/powlo.c
+++ Math/Calc/powlo.c
@@ -1,19 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   math.h                                             :+:    :+:            */
+/*   powlo.c                                            :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/05/31 13:42:30 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/05/31 13:50:07 by ehollidg      ########   odam.nl         */
+/*   Created: 2019/05/31 13:45:15 by ehollidg       #+#    #+#                */
+/*   Updated: 2019/05/31 13:49:47 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATH_H
-# define MATH_H
+unsigned long        powlo(unsigned long base, unsigned long expo)
+{
+    unsigned long i;
 
-//Calc
-unsigned long        powlo(unsigned long base, unsigned long expo);
-
-#endif
+    i = base;
+    while (expo > 0)
+    {
+        i *= base;
+        expo--;
+    }
+    return (i);
+}
