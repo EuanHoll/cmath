490e5ac864c498287e5f493d5815b4e744c168d5
Euan Hollidge <ehollidg@f0r1s3.codam.nl>
2019-05-31 13:53:24 +0200

    powint

--- Math/Calc/powint.c
+++ Math/Calc/powint.c
@@ -1,20 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   math.h                                             :+:    :+:            */
+/*   powint.c                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/05/31 13:42:30 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/05/31 13:52:36 by ehollidg      ########   odam.nl         */
+/*   Created: 2019/05/31 13:51:02 by ehollidg       #+#    #+#                */
+/*   Updated: 2019/05/31 13:52:41 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATH_H
-# define MATH_H
+unsigned int    powint(unsigned int base, unsigned int expo)
+{
+    unsigned int i;
 
-//Calc
-unsigned long	powlo(unsigned long base, unsigned long expo);
-unsigned int	powint(unsigned int base, unsigned int expo);
-
-#endif
+    i = base;
+    while (expo > 0)
+    {
+        i *= base;
+        expo--;
+    }
+    return (i);
+}
--- Math/Calc/powlo.c
+++ Math/Calc/powlo.c
@@ -6,11 +6,11 @@
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/31 13:45:15 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/05/31 13:49:47 by ehollidg      ########   odam.nl         */
+/*   Updated: 2019/05/31 13:52:46 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned long        powlo(unsigned long base, unsigned long expo)
+unsigned long   powlo(unsigned long base, unsigned long expo)
 {
     unsigned long i;
 
