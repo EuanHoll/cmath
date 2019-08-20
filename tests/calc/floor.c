71efd7e9cfa800b1b4c4a49f8c2d9f6afef904f4
Euan <ehollidge@hotmail.com>
2019-08-20 15:58:55 +0200

    Floor Tests
    Added m_floor tests
    m_floor bug fixes

--- tests/calc/floor.c
+++ tests/calc/floor.c
@@ -5,12 +5,26 @@
 /*                                                     +:+                    */
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/05/31 14:34:21 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/08/20 15:57:07 by ehollidg      ########   odam.nl         */
+/*   Created: 2019/08/20 15:52:30 by ehollidg       #+#    #+#                */
+/*   Updated: 2019/08/20 15:54:15 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-long	m_floor(double val)
+#include "cmath.h"
+#include <criterion/criterion.h>
+#include <limits.h>
+
+Test(floor, zero)
 {
-	return (val >= 0 ? (long)val : (long)(val - 1));
+	cr_assert(m_floor(0) == 0);
+}
+
+Test(floor, positive)
+{
+	cr_assert(m_floor(5.4) == 5);
+}
+
+Test(floor, negative)
+{
+	cr_assert(m_floor(-5.4) == -6);
 }
