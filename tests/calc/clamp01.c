de9a7e597b771fd0443a65b2b37878f69b45dbee
Euan <ehollidge@hotmail.com>
2019-08-20 15:52:11 +0200

    Clamp01 Tests
    Added Clamp01 Tests
    Fixed Clamp01 bug

--- tests/calc/clamp01.c
+++ tests/calc/clamp01.c
@@ -5,14 +5,36 @@
 /*                                                     +:+                    */
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/05/31 15:03:04 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/08/20 15:51:09 by ehollidg      ########   odam.nl         */
+/*   Created: 2019/08/20 15:46:30 by ehollidg       #+#    #+#                */
+/*   Updated: 2019/08/20 15:50:44 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-double		m_clamp01(double val)
+#include "cmath.h"
+#include <criterion/criterion.h>
+#include <limits.h>
+
+Test(clamp01, zero)
 {
-	if (val >= 0 && val <= 1)
-		return (val);
-	return (val > 1 ? 1 : 0);
+	cr_assert(m_clamp01(0) == 0);
+}
+
+Test(clamp01, zero_minus)
+{
+	cr_assert(m_clamp01(-1) == 0);
+}
+
+Test(clamp01, one)
+{
+	cr_assert(m_clamp01(1) == 1);
+}
+
+Test(clamp01, one_positive)
+{
+	cr_assert(m_clamp01(2) == 1);
+}
+
+Test(clamp01, half)
+{
+	cr_assert(m_clamp01(0.5) == 0.5);
 }
