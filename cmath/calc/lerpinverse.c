78999249081685b8b789bad7dcf9c86bc143b77b
Euan <ehollidge@hotmail.com>
2019-08-21 15:31:06 +0200

    Lerp Inverse Tests
    Added unit tests for m_lerpinverse
    Fixed Lerp Inverse Logic

--- cmath/calc/lerpinverse.c
+++ cmath/calc/lerpinverse.c
@@ -6,7 +6,7 @@
 /*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/31 15:26:33 by ehollidg       #+#    #+#                */
-/*   Updated: 2019/08/20 14:45:29 by ehollidg      ########   odam.nl         */
+/*   Updated: 2019/08/21 15:28:02 by ehollidg      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 double		m_lerpinverse(double a, double b, double t)
 {
+	if (t == a)
+		return (0);
 	return (a != b ? m_clamp01((t - a) / (b - a)) : 0);
 }
---
+++ tests/calc/lerpinverse.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   lerpinverse.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ehollidg <ehollidg@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/08/21 15:16:42 by ehollidg       #+#    #+#                */
+/*   Updated: 2019/08/21 15:30:21 by ehollidg      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cmath.h"
+#include <criterion/criterion.h>
+#include <limits.h>
+#include <stdio.h>
+
+Test(lerpinverse, zero01)
+{
+	cr_assert(m_lerpinverse(0, 0, 0) == 0);
+}
+
+Test(lerpinverse, zero02)
+{
+	cr_assert(m_lerpinverse(-5, 5, -5) == 0);
+}
+
+Test(lerpinverse, zero03)
+{
+	cr_assert(m_lerpinverse(5, 10, 5) == 0);
+}
+
+Test(lerpinverse, positive01)
+{
+	cr_assert(m_lerpinverse(10, 20, 12) == 0.2);
+}
+
+Test(lerpinverse, positive02)
+{
+	cr_assert(m_lerpinverse(10, 50, 26) == 0.4);
+}
+
+Test(lerpinverse, positive03)
+{
+	cr_assert(m_lerpinverse(10, 100, 46) == 0.4);
+}
+
+Test(lerpinverse, negative01)
+{
+	cr_assert(m_lerpinverse(-10, -20, -14) == 0.4);
+}
+
+Test(lerpinverse, negative02)
+{
+	cr_assert(m_lerpinverse(-10, -50, -26) == 0.4);
+}
+
+Test(lerpinverse, negative03)
+{
+	cr_assert(m_lerpinverse(-10, -100, -46) == 0.4);
+}
+
+Test(lerpinverse, nega_pos_01)
+{
+	cr_assert(m_lerpinverse(-10, 20, 2) == 0.4);
+}
+
+Test(lerpinverse, nega_pos_02)
+{
+	cr_assert(m_lerpinverse(-10, 50, 14) == 0.4);
+}
+
+Test(lerpinverse, nega_pos_03)
+{
+	cr_assert(m_lerpinverse(-10, 100, 34) == 0.4);
+}
