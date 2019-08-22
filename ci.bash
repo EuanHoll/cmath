a20b65d5e1fa730b2a5c36fff43902d4a1de811c
Euan <ehollidge@hotmail.com>
2019-08-22 17:43:30 +0200

    Update ci.bash

--- ci.bash
+++ ci.bash
@@ -7,7 +7,7 @@ apt-get install -y criterion-dev
 make GCOV=TRUE LINK=TRUE
 ./tests/tests
 cd cmath
-gcov -f -b -c calc/*.c trig/*.c 1>/dev/null 2>/dev/null
+gcov -f -b -c calc/*.c trig/*.c
 if [[ $1 == TRUE ]]; then
 rm -rf *.c.gcov
 fi
