92112212bd6afae4855d524625ab91172d048a9a
Euan <ehollidge@hotmail.com>
2019-08-23 12:44:29 +0200

    Adding Mac Support

--- ci.bash
+++ ci.bash
@@ -1,9 +1,16 @@
 #!/bin/bash
-apt-get update
-apt-get install -y software-properties-common curl make gcc ggcov
-add-apt-repository -y ppa:snaipewastaken/ppa
-apt-get update
-apt-get install -y criterion-dev
+OS=`uname -s`
+if [[$OS == Linux]]; then
+	apt-get update
+	apt-get install -y software-properties-common curl make gcc ggcov
+	add-apt-repository -y ppa:snaipewastaken/ppa
+	apt-get update
+	apt-get install -y criterion-dev
+else
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	export HOMEBREW_NO_AUTO_UPDATE=1
+	brew install snaipe/soft/criterion
+fi
 make GCOV=TRUE LINK=TRUE
 ./tests/tests
 cd cmath
